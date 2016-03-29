f717dcaa4f827dfbc0f14c48af8b10ea361ed089
Quanyong Huang <quanyongh@foxmail.com>
2016-03-29 18:01:45 +0800

    renew

--- Code/VS2015/CppMirror/CppMirror/Main.cpp
+++ Code/VS2015/CppMirror/CppMirror/Main.cpp
@@ -13,7 +13,9 @@
 #include <boost/mpl/string.hpp>
 #include <boost/mpl/fold.hpp>
 #include <boost/mpl/size_t.hpp>
+#include <cstddef>
 
+#include <map>
 
 #define REM(...) __VA_ARGS__
 #define EAT(...)
@@ -44,6 +46,8 @@ struct make_const<const M, T>
 	typedef typename boost::add_const<T>::type type;
 };
 
+#define REFLECT(x)	\
+static std::map<std::string, int> * x::_attrIndex = nullptr;
 
 #define REFLECTABLE(...) \
 static const int _lenIndex = BOOST_PP_VARIADIC_SIZE(__VA_ARGS__); \
@@ -52,7 +56,11 @@ template<int N, class Self> \
 struct field_data {}; \
 BOOST_PP_SEQ_FOR_EACH_I(REFLECT_EACH, data, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))	\
 static const char* const _index[]; \
-int FindAttrIndex(const char* propName) \
+static std::map<std::string, int> * _attrIndex; \
+static void GenAttrIndex(){	\
+	return; \
+}	\
+int SelAttr(const char* propName) \
 {	\
 	for (int i = 0; i < _lenIndex; i++)	\
 	{	\
@@ -62,7 +70,7 @@ int FindAttrIndex(const char* propName) \
 	return -1;	\
 }	\
 magicVar GetAttr(const char* propName) {	\
-	int index = this->FindAttrIndex(propName);	\
+	int index = this->SelAttr(propName);	\
 	magicVar mv;	\
 	switch (index)	\
 	{	\
@@ -70,22 +78,23 @@ magicVar GetAttr(const char* propName) {	\
 	}	\
 	return mv; \
 }	\
-
-//Auto Gen
-//template<class T>
-//void SetAttr(const char* propName, T var) {
-//	int index = this->FindAttrIndex(propName);
-//	switch (index)
-//	{
-//	case _index4mProp0:
-//		this->_name4mProp0 = var;
-//		break;
-//	}
-//}
+template<class T>	\
+void SetAttr(const char* propName, T var) {	\
+	int index = this->SelAttr(propName);	\
+	switch (index)	\
+	{	\
+		BOOST_PP_SEQ_FOR_EACH_I(REFLECT_EACH_GEN_SET, data, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)) \
+	}\
+}
 
 #define REFLECT_EACH_GEN_GET(r, data, i, x) \
 case i:	\
 	mv = Transform(this->STRIP(x));\
+	break;\
+
+#define REFLECT_EACH_GEN_SET(r, data, i, x) \
+case i:	\
+	this->STRIP(x)=var;\
 	break;\
 
 #define REFLECT_EACH(r, data, i, x) \
@@ -138,6 +147,15 @@ magicVar Transform<std::string>(std::string var)
 	return mv;
 }
 
+template <>
+magicVar Transform<int>(int var)
+{
+	magicVar mv;
+	mv.type = mv.int_;
+	mv.value.intV = var;
+	return mv;
+}
+
 void printVar(magicVar var)
 {
 	switch (var.type)
@@ -162,79 +180,22 @@ void printVar(magicVar var)
 
 class TestObj {
 public:
-	std::string mProp0;
-	int mProp1;
-
-#define _index4mProp0 0
-#define _index4mProp1 1
-#define _name4mProp0 mProp0
-#define _name4mProp1 mProp1
-#define _type4mProp0 std::string
-#define _type4mProp1 int
-
-	//Auto Gen
-	static const char* const _index[];
-
-	//Auto Gen
-	int FindAttrIndex(const char* propName)
-	{
-		for (int i = 0; i < sizeof(*_index); i++)
-		{
-			if (_index[i] == propName)
-				return i;
-		}
-		return -1;
-	}
-
-	//Auto Gen
-	magicVar GetAttr(const char* propName) {
-		int index = this->FindAttrIndex(propName);
-		magicVar mv;
-		switch (index)
-		{
-		case _index4mProp0:
-			//_GetAttr<TestObj, _index4mProp0, _type4mProp0> GetAttr(this);
-			mv = Transform(this->_name4mProp0);
-			break;
-		}
-		return mv;
-	}
-
-	template<class T>
-	void SetAttr(const char* propName, T var) {
-		int index = this->FindAttrIndex(propName);
-		switch (index)
-		{
-		case _index4mProp0:
-			this->_name4mProp0 = var;
-			break;
-		}
-	}
-
-	template <class Self, class T>
-	struct _SetAttr
-	{
-		Self &self;
-		_SetAttr(Self &self) : self(self) {}
-		void operator()(const char* propName, T)
-		{
-			return;
-		}
-	};
+	REFLECTABLE(
+		(std::string) mProp0,
+		(int) mProp1
+	)
 
 	TestObj(const char* prop0, int prop1) :mProp0(prop0), mProp1(prop1){}
 
-private:
-
 };
 
+REFLECT(TestObj)
 const char* const TestObj::_index[] = { "mProp0", "mProp1","mProp3"};
 
 int main() {
-	//Person p("Tom", 82);
-	//print_fields(p);
 	auto obj = new TestObj("stringProp", 110);
-	printVar(obj->GetAttr("mProp0"));
+	obj->SetAttr("mProp1", 100);
+	printVar(obj->GetAttr("mProp1"));
 	//obj->SetAttr("mProp1", 1);
 	//std::count << obj->GetAttr("mProp1") << std::endl;
 	//std::cout << boost::hash_value("mProp1") << std::endl;
